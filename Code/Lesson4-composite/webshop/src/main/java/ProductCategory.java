a688577dae5d29b529b311a3824a0ca92dc94a8b
sophearyrin1 <srin@miu.edu>
2023-02-28 15:56:52 -0600

    lab4-g

---
+++ Code/Lesson4-composite/webshop/src/main/java/ProductCategory.java
@@ -0,0 +1,10 @@
+public abstract class ProductCategory {
+
+    protected String name;
+
+    public ProductCategory(String name) {
+        this.name = name;
+    }
+
+    public abstract void output();
+}
